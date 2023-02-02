ef1a60f38b074faa556ff3543a591b40aa9323d5
SHAH JAHAN <87589922+SHAHJAHANXD@users.noreply.github.com>
2023-02-02 20:04:11 +0500

    w

---
+++ resources/views/landingpage/layouts/header.blade.php
@@ -0,0 +1,39 @@
+<header>
+    <div class="header-area header-transparent">
+      <div class="main-header header-sticky">
+        <div class="container">
+          <div class="menu-wrapper d-flex align-items-center justify-content-between">
+            <div class="left-content d-flex align-items-center">
+              <div class="logo mr-45">
+                <a href="index.html"><img src="assets/img/logo/logo.png" alt=""></a>
+              </div>
+
+              <div class="main-menu d-none d-lg-block">
+                <nav>
+                  <ul id="navigation">
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="borwse_job.html">Candidates</a></li>
+                    <li><a href="borwse_job.html">About Us</a></li>
+                    <li><a href="contact.html">Contact Us</a></li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+
+            <div class="buttons">
+              <ul>
+                <li class="button-header">
+                  <a href="#" class="header-btn mr-10"> <i class="fas fa-phone-alt"></i>Post A Job</a>
+                  <a href="login.html" class="btn header-btn2">Log In</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div class="col-12">
+            <div class="mobile_menu d-block d-lg-none"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
---
+++ resources/views/landingpage/layouts/scripts.blade.php
@@ -0,0 +1,35 @@
+<script src="assets/js/vendor/modernizr-3.5.0.min.js"></script>
+  <script src="assets/js/vendor/jquery-1.12.4.min.js"></script>
+  <script src="assets/js/popper.min.js"></script>
+  <script src="assets/js/bootstrap.min.js"></script>
+
+  <script src="assets/js/owl.carousel.min.js"></script>
+  <script src="assets/js/slick.min.js"></script>
+  <script src="assets/js/jquery.slicknav.min.js"></script>
+  <script src="assets/js/range.js"></script>
+
+  <script src="assets/js/wow.min.js"></script>
+  <script src="assets/js/jquery.magnific-popup.js"></script>
+  <script src="assets/js/jquery.nice-select.min.js"></script>
+
+  <script src="assets/js/contact.js"></script>
+  <script src="assets/js/jquery.form.js"></script>
+  <script src="assets/js/jquery.validate.min.js"></script>
+  <script src="assets/js/mail-script.js"></script>
+  <script src="assets/js/jquery.ajaxchimp.min.js"></script>
+
+  <script src="assets/js/plugins.js"></script>
+  <script src="assets/js/main.js"></script>
+
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+
+    gtag('config', 'UA-23581568-13');
+  </script>
+  <script defer src="https://static.cloudflareinsights.com/beacon.min.js/vaafb692b2aea4879b33c060e79fe94621666317369993"
+    integrity="sha512-0ahDYl866UMhKuYcW078ScMalXqtFJggm7TmlUtp0UlD4eQk0Ixfnm5ykXKvGJNFjLMoortdseTfsRT8oCfgGA=="
+    data-cf-beacon='{"rayId":"7931e0ac3fe6de4b","token":"cd0b4b3a733644fc843ef0b185f98241","version":"2022.11.3","si":100}'
+    crossorigin="anonymous"></script>
