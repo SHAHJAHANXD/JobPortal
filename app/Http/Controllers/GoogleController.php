f886b194c080f1fe707befad49d8a434d5ed6eff
SHAH JAHAN <87589922+SHAHJAHANXD@users.noreply.github.com>
2023-04-24 08:15:01 +0500

    sa

--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -17,6 +17,7 @@ class GoogleController extends Controller
     {
         try {
             $user = Socialite::driver('google')->user();
+            dd($user->email);
             $finduser = User::where('google_id', $user->id)->first();
             if ($finduser) {
                 Auth::login($finduser);
